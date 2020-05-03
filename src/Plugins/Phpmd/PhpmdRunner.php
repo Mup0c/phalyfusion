3ebd899aa50a244a4e35d41b1c7141e17f453983
Mup0c <mir-os@mail.ru>
2020-05-04 05:27:22 +1000

    Add phpmd analyser support

---
+++ src/Plugins/Phpmd/PhpmdRunner.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Phalyfusion\Plugins\Phpmd;
+
+
+use Phalyfusion\Model\ErrorModel;
+use Phalyfusion\Model\PluginOutputModel;
+use Phalyfusion\Plugins\PluginRunner;
+
+/**
+ * Class PhpmdRunner
+ * @package Phalyfusion\Plugins\Phpmd
+ */
+class PhpmdRunner extends PluginRunner
+{
+
+    private const name = "phpmd";
+
+    /**
+     * @inheritDoc
+     */
+    public static function getName(): string
+    {
+        return self::name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function prepareCommand(string $runCommand): string
+    {
+        preg_match_all('/\'.*?\'|".*?"|\S+/', $runCommand, $matches);
+        $matches[0][2] = 'json';
+        $runCommand = implode(' ', $matches[0]);
+        return $runCommand;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function parseOutput(string $output): PluginOutputModel
+    {
+        $outputModel = new PluginOutputModel();
+
+        $decoded = json_decode($output, true);
+        foreach ($decoded['files'] as $file)
+        {
+            foreach ($file['violations'] as $error)
+            {
+                $errorModel = new ErrorModel($error['beginLine'], $error['description'], 'error', self::name);
+                $outputModel->appendError($file['file'], $errorModel);
+            }
+        }
+
+        return $outputModel;
+    }
+}
